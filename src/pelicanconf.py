777f858d18fb3266e881f1573baba150e7ac3e53
wudixiaop <laiming_5433@163.com>
2014-08-31 01:29:31 +0800

    test update base on pelican
    test update base on pelican

---
+++ src/pelicanconf.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Rocky Lai'
+SITENAME = u"Rocky Lai's Blog"
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'China/Beijing'
+
+DEFAULT_LANG = u'zh-cn'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Blogroll
+LINKS = ()
+
+# Social widget
+SOCIAL = ()
+
+DEFAULT_PAGINATION = 8
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
